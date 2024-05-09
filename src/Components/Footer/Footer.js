cb59d49186cede1a98f45d614404ad52bdb1c34e
JA MacBPro <153228808+joshayala@users.noreply.github.com>
2024-05-09 17:24:00 -0400

    Netlify debug pt3

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -4,7 +4,7 @@ import styles from "./Footer.module.css"
 function Footer () {
     return (
         <div className={styles.footer}>
-            <p>Enjoy! Made by <a href="https://joshayala.github.io/portfolio_website/" target="_blank">JA</a></p>
+            <p>Enjoy! Made by <a href="https://joshayala.github.io/portfolio_website/" target="_blank" rel="noreferrer">JA</a></p>
         </div>
     )
 }
